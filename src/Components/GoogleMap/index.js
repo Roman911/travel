fedcc53f075eba22b7a923375662d3bc8e8f184e
Roman Lysyk <roma-lysyk@ukr.net>
2019-11-22 19:25:43 +0200

    added maps

--- src/Components/GoogleMap/index.js
+++ src/Components/GoogleMap/index.js
@@ -64,7 +64,7 @@ export default function () {
     <Background />
     <div style={{ width: '1000px', margin: '0 auto' }}>
       <WrappedMap
-        googleMapURL = { 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDLRRgxqKe9Ok-an59Hh7qxfKZG0mGqHW8' }
+        googleMapURL = { `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}` }
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `600px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
--- src/Components/index.js
+++ src/Components/index.js
@@ -3,4 +3,4 @@ export { default as Home } from './Home';
 export { default as Article } from './Article';
 export { default as Map } from './GoogleMap';
 export { default as About } from './About';
-export { default as Login } from './Login';
+export { default as Login } from '../modules/LoginForm/Components';
